0837f187cb8dbf6d34f726eafaa83d2bfd2c6f7e
oigen88 <sazukinpavelal@gmail.com>
2023-03-18 17:05:30 +0300

    change telegraf to node-telegram-bot-api

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
-import { Telegraf } from "telegraf";
+import TelegramBot from "node-telegram-bot-api";
 
-const bot = new Telegraf("6163566945:AAEU4D8JfyzcJ4LHliekDLmUIOxo4_OUyZY");
+const bot = new TelegramBot('6163566945:AAEU4D8JfyzcJ4LHliekDLmUIOxo4_OUyZY', {polling: true});
 
-bot.start(() => {
-  console.log("start");
+bot.onText(/\/echo (.+)/, (msg, match) => {
+    const chatId = msg.chat.id;
+    const resp = match?.[1]|| 'bad'; // the captured "whatever"
+    bot.sendMessage(chatId, resp+chatId);
 });
 
-bot.hears("a", (data) => {
-  console.log(data);
-});
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
 
-bot.launch()
+    bot.sendMessage(chatId, 'Received your message');
+});
