06e77bb1e95ba12ab6fe70252fb983066cfdf102
oigen88 <sazukinpavelal@gmail.com>
2023-03-19 19:02:10 +0300

    delete logs

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
-import App from "./src/app.js";
+import App from "./app.js";
 
 async function main() {
-  console.log('start');
   const app = new App("6163566945:AAEU4D8JfyzcJ4LHliekDLmUIOxo4_OUyZY");
 
   await app.start();
