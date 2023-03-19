19dac47b943e3f54d2d9f3e960668f67f0df7603
oigen88 <sazukinpavelal@gmail.com>
2023-03-19 19:01:55 +0300

    refactor app ts

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import TelegramBot from "node-telegram-bot-api";
 import * as mongoose from "mongoose";
-import { Note } from "./schemas/Note.js";
+import AddCommand from "./commands/AddCommand.js";
+import ListCommand from "./commands/ListCommand.js";
+import StartCommand from "./commands/StartCommand.js";
 
 class App {
   private telegramId: string;
@@ -16,57 +18,17 @@ class App {
       await mongoose.connect("mongodb://mongo:27017/notes");
       this.bot = new TelegramBot(this.telegramId, { polling: true });
       this.configureBot();
-      console.log("good");
+      console.log("connection successfully");
     } catch (e) {
       console.log("ERROR:", e);
     }
   }
 
   configureBot() {
-    this.bot.onText(/\/start/, (msg) => {
-      this.bot.sendMessage(msg.chat.id, "some message", {
-        reply_markup: {
-          keyboard: [
-            [
-              {
-                text: "list",
-              },
-            ],
-            [
-              {
-                text: "add",
-              },
-            ],
-            [
-              {
-                text: "help",
-              },
-            ],
-          ],
-        },
-      });
-    });
+    const commands = [new AddCommand(), new ListCommand(), new StartCommand()];
 
-    this.bot.onText(/list/, (msg) => {
-      this.bot.sendMessage(msg.chat.id, "you send list");
-    });
-
-    this.bot.onText(/add/, async (msg) => {
-      const message = await this.bot.sendMessage(msg.chat.id, "3");
-      this.bot.onReplyToMessage(
-        msg.chat.id,
-        message.message_id,
-        async (msg) => {
-          const note = new Note({ text: msg.text });
-          await note.save();
-        }
-      );
-    });
-    this.bot.onText(/list/, async (msg) => {
-      const notes = await Note.find();
-      console.log(notes);
-      console.log(notes.join(" "));
-      this.bot.sendMessage(msg.chat.id, notes.join(" "));
+    commands.forEach((command) => {
+      this.bot.onText(command.route, (msg) => command.execute(this.bot, msg));
     });
   }
 }
